a4f50c252e10aee4d99cea299d74d20211ac5f59
Teiresias22 <teiresias1987@gmail.com>
2021-11-18 16:02:02 +0900

    Initial Commit

---
+++ SeSAC.Heritage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC.Heritage
+//
+//  Created by Joonhwan Jeon on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
