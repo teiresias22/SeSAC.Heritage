71b52ed4a404db777074cd782e272030bcea9564
Teiresias22 <teiresias1987@gmail.com>
2021-12-13 19:41:34 +0900

    List Page Top Button Custom

--- SeSAC.Heritage/VC/ListViewController.swift
+++ SeSAC.Heritage/VC/ListViewController.swift
@@ -2,7 +2,15 @@ import UIKit
 import RealmSwift
 
 class ListViewController: UIViewController {
-    @IBOutlet weak var TabBarButtonCollection: UICollectionView!
+    
+    @IBOutlet weak var topButtonView: UIView!
+    @IBOutlet weak var topLeftButton: UIButton!
+    @IBOutlet weak var topLeftUnderBarView: UIView!
+    @IBOutlet weak var topLeftBackground: UIView!
+    @IBOutlet weak var topRightButton: UIButton!
+    @IBOutlet weak var topRightUnderBarView: UIView!
+    @IBOutlet weak var topRightBackground: UIView!
+    
     @IBOutlet weak var listCollectionView: UICollectionView!
     
     let listInformation = ListInformation()
@@ -10,44 +18,77 @@ class ListViewController: UIViewController {
     let cityInformation = CityInformation()
     var target: String = "StockCode"
     
+    var buttonActive = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "JHeritage".localized()
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "MapoFlowerIsland", size: 20)!]
+                
+        let nibName = UINib(nibName: ListCategoryCollectionViewCell.identifier, bundle: nil)
+        listCollectionView.register(nibName, forCellWithReuseIdentifier: ListCategoryCollectionViewCell.identifier)
         
-        let nibName1 = UINib(nibName: ListCollectionViewCell.identifier, bundle: nil)
-        TabBarButtonCollection.register(nibName1, forCellWithReuseIdentifier: ListCollectionViewCell.identifier)
-        
-        let nibName2 = UINib(nibName: ListCategoryCollectionViewCell.identifier, bundle: nil)
-        listCollectionView.register(nibName2, forCellWithReuseIdentifier: ListCategoryCollectionViewCell.identifier)
-        
-        TabBarButtonCollection.allowsMultipleSelection = false
-
          // Do any additional setup after loading the view.
         listCollectionView.delegate = self
-        TabBarButtonCollection.delegate = self
-        
         listCollectionView.dataSource = self
-        TabBarButtonCollection.dataSource = self
         
+        topRightButtonSet()
+        topLeftButtonSet()
+        
+        topButtonView.backgroundColor = .clear
         listCollectionViewSet()
-        TabBarButtonCollectionViewSet()
         
         //print(">>\(UserDefaults.standard.object(forKey: "AppleLanguages"))<<")
     }
-    func TabBarButtonCollectionViewSet() {
-        let layout = UICollectionViewFlowLayout()
-        let spacing: CGFloat = 4
-        let width = TabBarButtonCollection.frame.width - (spacing * 3)
+    
+    func topRightButtonSet(){
+        topRightButton.setTitle(listInformation.list[0].title, for: .normal)
+        if buttonActive {
+            topRightButton.titleLabel?.font = UIFont(name: "MapoFlowerIsland", size: 14)
+            topRightButton.tintColor = .customBlack
+            topRightUnderBarView.backgroundColor = .clear
+            topRightBackground.backgroundColor = .clear
+            
+        } else {
+            topRightButton.titleLabel?.font = UIFont(name: "MapoFlowerIsland", size: 16)
+            topRightButton.tintColor = .customBlue
+            topRightUnderBarView.backgroundColor = .customBlue
+            topRightBackground.backgroundColor = .white
+        }
+    }
+    
+    func topLeftButtonSet() {
+        topLeftButton.setTitle(listInformation.list[1].title, for: .normal)
+        if buttonActive {
+            topLeftButton.titleLabel?.font = UIFont(name: "MapoFlowerIsland", size: 16)
+            topLeftButton.tintColor = .customBlue
+            topLeftUnderBarView.backgroundColor = .customBlue
+            topLeftBackground.backgroundColor = .white
+        } else {
+            topLeftButton.titleLabel?.font = UIFont(name: "MapoFlowerIsland", size: 14)
+            topLeftButton.tintColor = .customBlack
+            topLeftUnderBarView.backgroundColor = .clear
+            topLeftBackground.backgroundColor = .clear
+        }
+    }
+    
+    @IBAction func topLeftButtonClicked(_ sender: UIButton) {
+        buttonActive = true
+        target = listInformation.list[0].target
         
-        layout.itemSize = CGSize(width: width / 2.2, height: 42)
-        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
-        layout.minimumInteritemSpacing = spacing
-        layout.minimumLineSpacing = spacing
-        layout.scrollDirection = .horizontal
+        listCollectionView.reloadData()
+        topRightButtonSet()
+        topLeftButtonSet()
+    }
+    
+    
+    @IBAction func topRightButtonClicked(_ sender: UIButton) {
+        buttonActive = false
+        target = listInformation.list[1].target
         
-        TabBarButtonCollection.collectionViewLayout = layout
-        TabBarButtonCollection.backgroundColor = .clear
+        listCollectionView.reloadData()
+        topRightButtonSet()
+        topLeftButtonSet()
     }
     
     
@@ -69,86 +110,60 @@ class ListViewController: UIViewController {
 
 extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == TabBarButtonCollection {
-            return listInformation.list.count
-        } else {
-            if target == "StockCode" {
-                return stockCodeInformation.stockCode.count
-            }else {
-                return cityInformation.city.count
-            }
+        if target == "StockCode" {
+            return stockCodeInformation.stockCode.count
+        }else {
+            return cityInformation.city.count
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == TabBarButtonCollection {
-            guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: ListCollectionViewCell.identifier, for: indexPath) as? ListCollectionViewCell else { return UICollectionViewCell() }
-            item.backgroundColor = .customBlue
-            
-            item.listTitle.font = UIFont().MapoFlowerIsland14
-            item.listTitle.textAlignment = .center
-            item.listTitle.textColor = .customBlack
-            item.listTitle.text = listInformation.list[indexPath.row].title.localized()
-            
-            item.cellView.backgroundColor = .clear
-            
-            return item
-        } else {
-            guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: ListCategoryCollectionViewCell.identifier, for: indexPath) as? ListCategoryCollectionViewCell else { return UICollectionViewCell() }
-            item.backgroundColor = .clear
-            
-            if target == "StockCode" {
-                if let url = URL(string: stockCodeInformation.stockCode[indexPath.row].image){
-                    item.listCategoryImage.kf.setImage(with: url)
-                } else {
-                    item.listCategoryImage.image = UIImage(systemName: "star")
-                    item.listCategoryImage.backgroundColor = .customYellow
-                }
-                item.listCategoryLabel.text = stockCodeInformation.stockCode[indexPath.row].text.lowercased()
-                item.listCategoryImage.layer.borderWidth = 2
-                item.listCategoryImage.layer.borderColor = UIColor.customYellow?.cgColor
-            } else if target == "City" {
-                if let url = URL(string: cityInformation.city[indexPath.row].image){
-                    item.listCategoryImage.kf.setImage(with: url)
-                } else {
-                    item.listCategoryImage.image = UIImage(systemName: "star")
-                    item.listCategoryImage.backgroundColor = .customBlue
-                }
-                item.listCategoryLabel.text = cityInformation.city[indexPath.row].city.lowercased()
-                item.listCategoryImage.layer.borderWidth = 2
-                item.listCategoryImage.layer.borderColor = UIColor.customBlue?.cgColor
+        guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: ListCategoryCollectionViewCell.identifier, for: indexPath) as? ListCategoryCollectionViewCell else { return UICollectionViewCell() }
+        item.backgroundColor = .clear
+        
+        if target == "StockCode" {
+            if let url = URL(string: stockCodeInformation.stockCode[indexPath.row].image){
+                item.listCategoryImage.kf.setImage(with: url)
+            } else {
+                item.listCategoryImage.image = UIImage(systemName: "star")
+                item.listCategoryImage.backgroundColor = .customYellow
             }
-            item.listCategoryLabel.font = UIFont().MapoFlowerIsland14
-            
-            let width = item.frame.width
-            item.listCategoryImage.layer.cornerRadius = width/2
-            
-            return item
+            item.listCategoryLabel.text = stockCodeInformation.stockCode[indexPath.row].text.lowercased()
+            item.listCategoryImage.layer.borderWidth = 2
+            item.listCategoryImage.layer.borderColor = UIColor.customYellow?.cgColor
+        } else if target == "City" {
+            if let url = URL(string: cityInformation.city[indexPath.row].image){
+                item.listCategoryImage.kf.setImage(with: url)
+            } else {
+                item.listCategoryImage.image = UIImage(systemName: "star")
+                item.listCategoryImage.backgroundColor = .customBlue
+            }
+            item.listCategoryLabel.text = cityInformation.city[indexPath.row].city.lowercased()
+            item.listCategoryImage.layer.borderWidth = 2
+            item.listCategoryImage.layer.borderColor = UIColor.customBlue?.cgColor
         }
+        item.listCategoryLabel.font = UIFont().MapoFlowerIsland14
+        
+        let width = item.frame.width
+        item.listCategoryImage.layer.cornerRadius = width/2
+        
+        return item
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-
-        if collectionView == TabBarButtonCollection {
-            target = listInformation.list[indexPath.row].target
-            
-            TabBarButtonCollection.reloadData()
-            listCollectionView.reloadData()
+        let sb = UIStoryboard(name: "ListTable", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "ListTableViewController") as! ListTableViewController
+        
+        if target == "StockCode" {
+            let row = stockCodeInformation.stockCode[indexPath.row]
+            vc.stockCodeData = row
         } else {
-            let sb = UIStoryboard(name: "ListTable", bundle: nil)
-            let vc = sb.instantiateViewController(withIdentifier: "ListTableViewController") as! ListTableViewController
-            
-            if target == "StockCode" {
-                let row = stockCodeInformation.stockCode[indexPath.row]
-                vc.stockCodeData = row
-            } else {
-                let row = cityInformation.city[indexPath.row]
-                vc.cityData = row
-            }
-            
-            vc.listInformation = target
-             self.navigationController?.pushViewController(vc, animated: true)
+            let row = cityInformation.city[indexPath.row]
+            vc.cityData = row
         }
+        
+        vc.listInformation = target
+         self.navigationController?.pushViewController(vc, animated: true)
     }
     
 }
