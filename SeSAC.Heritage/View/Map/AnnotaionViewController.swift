b4bdba029515403c89769083a57e1e86cc83c0dd
Teiresias22 <teiresias1987@gmail.com>
2022-02-27 03:19:13 +0900

    ContainerView 제작을 위한 뷰 작성

---
+++ SeSAC.Heritage/View/Map/AnnotaionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AnnotaionViewController.swift
+//  SeSAC.Heritage
+//
+//  Created by Joonhwan Jeon on 2022/02/27.
+//
+
+import UIKit
+
+class AnnotaionViewController: BaseViewController {
+    
+}
